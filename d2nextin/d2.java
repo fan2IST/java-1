25c4bbc801e74e749f31c0fe3453e16874abd68f
fan2IST <fanISTREC2DX@outlook.com>
2022-06-23 22:15:02 +0800

    d2c

--- d2nextin/d2.java
+++ d2nextin/d2.java
@@ -1,5 +1,11 @@
 package d2nextin;
 
 public class d2 {
-
+    public static void main(String[] args) {
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        int ago = s.nextInt();
+        String str = "cn0e1";
+        System.out.println(str);
+        System.out.println(ago);
+    }
 }
