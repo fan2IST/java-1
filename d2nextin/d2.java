49ca6ef3f63ef9af791c236fec6db78a86f6c910
fan2IST <fanISTREC2DX@outlook.com>
2022-06-23 23:53:46 +0800

    d2d

--- d2nextin/d2.java
+++ d2nextin/d2.java
@@ -3,9 +3,22 @@ package d2nextin;
 public class d2 {
     public static void main(String[] args) {
         java.util.Scanner s = new java.util.Scanner(System.in);
-        int ago = s.nextInt();
+        int day = s.nextInt();
         String str = "cn0e1";
         System.out.println(str);
-        System.out.println(ago);
+        switch (day) {
+            case 1:
+                System.out.println("星期一");
+                break;
+            case 2:
+                System.out.println("星期二");
+                break;
+            default:
+                System.out.println("你说你妈那");
+        }
+        do {
+            System.out.println(day);
+            day++;
+        } while (day < 19);
     }
 }
