79e9ff60a3c62e1de907f4c0193c867f103ae817
fan2IST <fanISTREC2DX@outlook.com>
2022-06-23 18:27:56 +0800

    d2

--- d2datatype/d2.java
+++ d2datatype/d2.java
@@ -3,6 +3,8 @@ package d2datatype;
 public class d2 {
     public static void main(String[] args) {
         System.out.println("第一条测试");// 无意义测试
-
+        byte a = 3;
+        int b = 10;
+        System.out.println(a + b);
     }
 }
