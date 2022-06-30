57ed6a71d9a70be4958b431c0bd1003c750591e2
fan2IST <fanISTREC2DX@outlook.com>
2022-06-30 17:15:13 +0800

    d3-1

---
+++ d3class/d3.java
@@ -0,0 +1,23 @@
+package d3class;
+
+public class d3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int retValue = sumInt(a, b);
+        System.out.println("retValue =" + retValue);
+
+    }
+
+    public static int sumInt(int i, int j) {
+        int result = i + j;
+        int num = 3;
+        int retValue = divide(result, num);
+        return retValue;
+    }
+
+    public static int divide(int x, int y) {
+        int z = x / y;
+        return z;
+    }
+}
