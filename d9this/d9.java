6dd696b3a3d565b3864c2010b3a22c8cf84cb6af
fanIST <fanISTREC2DX@outlook.com>
2022-08-02 16:30:24 +0800

    d91

---
+++ d9this/d9.java
@@ -0,0 +1,11 @@
+package d9this;
+
+public class d9 {
+    int num = 10;
+
+    public static void main(String[] args) {
+        d9 tt = new d9();
+        System.out.println(tt.num);
+
+    }
+}
---
+++ d9this2/d92.java
@@ -0,0 +1,7 @@
+package d9this2;
+
+public class d92 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
