7ba80a625e31fc573164f5fdda70899fe62537f3
fanIST <fanISTREC2DX@outlook.com>
2022-08-10 23:58:03 +0800

    d11-2

---
+++ d10familyaccount/d103.java
@@ -0,0 +1,18 @@
+package d10familyaccount;
+
+public class d103 {
+    public static void main(String[] args) {
+        boolean isFlag = true;
+        while (isFlag) {
+            System.out.println("------------------------家庭收支记账-------------------------");
+            System.out.println("    ");
+            System.out.println("                        1.收支明细");
+            System.out.println("                        2.登记收入");
+            System.out.println("                        3.登记支出");
+            System.out.print("                        4.退    出\n");
+            System.out.print("             请选择----(1-4):");
+            break;
+        }
+    }
+
+}
