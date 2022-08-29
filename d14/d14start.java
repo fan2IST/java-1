0df0c58a4fd644bccdac6816bea3f38262a485db
fanIST <fanISTREC2DX@outlook.com>
2022-08-29 11:04:07 +0800

    d16-2

--- d14/d14start.java
+++ d14/d14start.java
@@ -5,6 +5,14 @@ public class d14start extends Thread {// 调用线程
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("my thread");
+
+        }
+        int b[][] = new int[][] { { 1 }, { 2, 3 }, { 4, 5, 6 } };
+        for (int k = 0; k < b.length; k++) {
+            for (int c = 0; c < b[k].length; c++) {
+                System.out.println(b[k][c]);
+            }
+
         }
     }
 }
---
+++ d15/d15.java
@@ -0,0 +1,8 @@
+package d15;
+
+public class d15 {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello");
+    }
+}
