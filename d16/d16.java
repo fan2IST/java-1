9437110efd97a4801272d681673322229cb6ec4c
fanIST <fanISTREC2DX@outlook.com>
2022-08-29 11:03:55 +0800

    d16

---
+++ d16/d16.java
@@ -0,0 +1,12 @@
+package d16;
+
+import java.util.Random;
+
+public class d16 {
+    public static void main(String[] args) {
+        Random dom = new Random();
+        int test = dom.nextInt(20);
+        System.out.println(test);
+
+    }
+}
