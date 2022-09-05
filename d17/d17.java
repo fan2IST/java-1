fd30ca40b970b2ae14fc1541f6b4ba0e194279cc
fanIST <fanISTREC2DX@outlook.com>
2022-09-05 10:36:24 +0800

    d17-2

---
+++ d17/d17.java
@@ -0,0 +1,14 @@
+package d17;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class d17 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList<>();
+        list.add("1122");
+        list.add("0033");
+        list.add("44455");
+        System.out.println(list);
+    }
+}
