9234da8f942f17bb7a7c8311b98d4f4867c1c31a
fanIST <fanISTREC2DX@outlook.com>
2022-09-06 10:16:15 +0800

    d17-1

--- d17/d17.java
+++ d17/d17.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 
 public class d17 {
     public static void main(String[] args) {
-        ArrayList list = new ArrayList<>();
-        list.add("1122");
-        list.add("0033");
-        list.add("44455");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(98);
+        list.add(97);
+        list.add(96);
+
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) <= 80) {
+                list.remove(i);
+            }
+        }
         System.out.println(list);
     }
 }
