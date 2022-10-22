9085e2e564cab15b763c3bec2dff91d89429f1f9
fanIST <fanISTREC2DX@outlook.com>
2022-10-22 14:49:07 +0800

    d33

--- d30/d30.java
+++ d30/d30.java
@@ -2,7 +2,7 @@
  * @Author: fanIST fanISTREC2DX@outlook.com
  * @Date: 2022-10-09 08:54:01
  * @LastEditors: fanIST fanISTREC2DX@outlook.com
- * @LastEditTime: 2022-10-09 09:26:21
+ * @LastEditTime: 2022-10-09 09:46:44
  * @FilePath: \java-1\d30\d30.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,15 +14,18 @@ public class d30 {
         int d;
         for (; a < 100; a++) {
             for (; b < 100; b++) {
-                for (; c < 100; c += 3) {
+                for (; c < 100;) {
                     d = a + b + 3 * c;
                     if (d == 100) {
                         int f;
                         f = 5 * a + 3 * b + c;
                         if (f == 100) {
                             System.out.println(a + " " + b + " " + 3 * c + " ");
+                            break;
+
                         }
                     }
+                    c = c + 3;
                 }
                 c = 0;
             }
